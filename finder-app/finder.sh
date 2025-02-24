1eed6a6934d66ec8f681b9586a986a7bd00ecaa5
Alireza <alirezabeigimech@gmail.com>
2025-02-24 09:25:30 +0000

    success unit_test

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,7 +1,30 @@
-set -e
-set -u
+#!/bin/bash
 
-WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+searchstr=$2  # Assign second argument to variable searchstr
 
-echo "Using default value ${WRITESTR} for string to write"
+
+dir=$1
+
+if [ -d "$dir" ]; then
+    echo "Directory '$dir' exists."
+    file_count=$(ls -1 "$dir" | wc -l)
+    
+else
+    echo "Directory '$dir' does not exist."
+    exit 1
+fi
+
+if [ -n "$2" ]
+then
+    
+    
+    matching_lines=$(grep -r -c "$searchstr" "$dir" | awk -F: '{sum += $2} END {print sum}')
+    
+    echo "The number of files are $file_count and the number of matching lines are $matching_lines"
+else
+    echo "Variable does not exist."
+    exit 1
+fi
+exit 0
+
+
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,4 +1,33 @@
-WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+#!/bin/bash
 
-echo "Using default value ${WRITESTR} for string to write"
+
+# Check if both arguments are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Error: Both the file path and text string are required."
+    exit 1
+fi
+
+# Get the full path to the file and the text string to write
+writefile="$1"
+writestr="$2"
+
+# Create the directory path if it doesn't exist
+dirpath=$(dirname "$writefile")
+if [ ! -d "$dirpath" ]; then
+    echo "Directory does not exist. Creating directory..."
+    mkdir -p "$dirpath"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create directory $dirpath."
+        exit 1
+    fi
+fi
+
+# Write the string to the file, overwriting any existing file
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to write to file $writefile."
+    exit 1
+fi
+
+echo "File '$writefile' has been created/overwritten with the provided content."
+exit 0
